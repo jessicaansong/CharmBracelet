b007e89cc368faaa375e63158f14146a8c009c3c
jessicaansong <jessica.ansong@gmail.com>
2015-10-01 13:11:00 -0400

    JS Exercises

---
+++ Charm/JavaScriptExamples/factorial.js
@@ -0,0 +1,9 @@
+﻿function factorial() {
+    var f = document.getElementById('f').value;
+    for (i = 1; i < f; i--) {
+    factResult *= i;
+    }
+    var displayNow = document.getElementById('factResult');
+
+   displayNow.innerHTML = "The factorial is " + factResult;
+}
---
+++ Charm/JavaScriptExamples/fizz.js
@@ -0,0 +1,27 @@
+﻿function fizzBuzz() {
+    var fb1 = document.getElementById('fb1').value;
+    var fb2 = document.getElementById('fb2').value;
+    var statement;
+    var result = document.getElementById('result');
+
+    if (fb1 < 1 || fb1 >= 101 || fb2 < 1 || fb2 >= 101)
+         statement = "Please enter values between 1 and 100.";
+
+    else {
+        for (n = 1; n <= 100; n++)
+            if (n % fb1 * fb2)
+                var onMonitor = FizzBuzz;
+            else
+            {
+                result = " ";
+            }
+
+
+    }
+
+    var onMonitor = document.getElementById('statement');
+
+
+    onMonitor.innerHTML = statement;
+ 
+}
